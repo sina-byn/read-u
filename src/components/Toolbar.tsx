0e5419a6e071dba502918839f141d034b55646ae
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-27 10:06:12 +0330

    toolbar component added

---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,12 @@
+const Toolbar = () => {
+  return (
+    <header className='toolbar h-16 bg-primary'>
+      <div className='i-container flex items-center justify-between gap-x-6'>
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
+    </header>
+  );
+};
+
+export default Toolbar;
