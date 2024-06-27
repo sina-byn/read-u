e2b9bac5d3edf8f892549191558f9b3691a18f74
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-27 18:56:19 +0330

    zIndex prop added to modal

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -11,12 +11,20 @@ import useClient from '@/hooks/useClient';
 type ModalProps = {
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
+  zIndex?: number;
   closeable?: boolean;
   className?: string;
   children: React.ReactNode | ((closeHandler: () => void) => React.ReactNode);
 };
 
-const Modal = ({ open, setOpen, className, children, closeable = true }: ModalProps) => {
+const Modal = ({
+  open,
+  setOpen,
+  className,
+  children,
+  zIndex = 50,
+  closeable = true,
+}: ModalProps) => {
   const isClient = useClient();
 
   const closeHandler = () => closeable && setOpen(false);
@@ -31,8 +39,9 @@ const Modal = ({ open, setOpen, className, children, closeable = true }: ModalPr
   return isClient && open
     ? createPortal(
         <div
+          style={{ zIndex }}
           onClick={backdropClickHandler}
-          className='modal-backdrop flex items-center justify-center fixed inset-0 z-50 bg-black/50'
+          className='modal-backdrop flex items-center justify-center fixed inset-0 bg-black/50'
         >
           <div className={cn('modal-wrapper size-fit', className)} onClick={clickHandler}>
             {typeof children === 'function' ? children(closeHandler) : children}
