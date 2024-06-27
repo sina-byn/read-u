eaf5275d8d052da339c1a82f979de131e127e7ae
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-27 22:15:19 +0330

    view toggle ui fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+// * hooks
+import { useAppContext } from '@/context/AppContext';
+
 // * components
 import Tabs from '@/components/Tabs';
 import Editor from '@/components/Editor';
@@ -5,14 +10,29 @@ import RawMarkdown from '@/components/RawMarkdown';
 import MarkdownDisplay from '@/components/MarkdownDisplay';
 
 const Home = () => {
+  const { view } = useAppContext();
+
   return (
     <main className='py-8'>
       <div className='i-container h-full'>
-        <Tabs heads={['editor', 'preview', 'raw']}>
-          <Editor />
-          <MarkdownDisplay />
-          <RawMarkdown />
-        </Tabs>
+        {view === 'split' && (
+          <div className='split-view grid grid-cols-2 gap-x-6 h-full'>
+            <Tabs heads={['editor']}>
+              <Editor />
+            </Tabs>
+            <Tabs heads={['preview', 'raw']}>
+              <MarkdownDisplay />
+              <RawMarkdown />
+            </Tabs>
+          </div>
+        )}
+        {view === 'tabs' && (
+          <Tabs heads={['editor', 'preview', 'raw']}>
+            <Editor />
+            <MarkdownDisplay />
+            <RawMarkdown />
+          </Tabs>
+        )}
       </div>
     </main>
   );
