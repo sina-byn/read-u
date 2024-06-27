3f7ee8c933704404351f80106fb455526f2ab692
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-27 22:08:29 +0330

    home page components and layout added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+// * components
+import Tabs from '@/components/Tabs';
+import Editor from '@/components/Editor';
+import RawMarkdown from '@/components/RawMarkdown';
+import MarkdownDisplay from '@/components/MarkdownDisplay';
+
 const Home = () => {
-  return <main></main>;
+  return (
+    <main className='py-8'>
+      <div className='i-container h-full'>
+        <Tabs heads={['editor', 'preview', 'raw']}>
+          <Editor />
+          <MarkdownDisplay />
+          <RawMarkdown />
+        </Tabs>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
