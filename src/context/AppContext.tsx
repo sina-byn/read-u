56bcabf8ac6c5fe5fe43edebcdab96726695e333
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-27 16:28:55 +0330

    theme field added to AppContext

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -6,6 +6,8 @@ import { useState, useContext, createContext, type Dispatch, type SetStateAction
 const appContext = createContext<AppContext | null>(null);
 
 // * types
+import type { Theme } from '@/components/Toolbar';
+
 type View = 'tabs' | 'split';
 
 type ProviderProps = { children: React.ReactNode };
@@ -13,12 +15,15 @@ type ProviderProps = { children: React.ReactNode };
 type AppContext = {
   view: View;
   setView: Dispatch<SetStateAction<View>>;
+  theme: Theme;
+  setTheme: Dispatch<SetStateAction<Theme>>;
 };
 
 const AppContextProvider = ({ children }: ProviderProps) => {
   const [view, setView] = useState<View>('split');
+  const [theme, setTheme] = useState<Theme>('light');
 
-  const context = { view, setView };
+  const context = { view, setView, theme, setTheme };
 
   return <appContext.Provider value={context}>{children}</appContext.Provider>;
 };
