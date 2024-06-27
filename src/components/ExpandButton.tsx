869a8a27ca14068408033f85fab6ff43138e6c32
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-27 19:12:52 +0330

    expand-button component added

---
+++ src/components/ExpandButton.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+// * components
+import Modal from './ui/Modal';
+import Button from './ui/Button';
+
+// * icons
+import { X, Expand } from 'lucide-react';
+
+// * types
+type ExpandButtonProps = { title?: string; children: JSX.Element };
+
+const ExpandButton = ({ title, children }: ExpandButtonProps) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const toggleHandler = () => setOpen(prev => !prev);
+
+  return (
+    <Button
+      variant='secondary'
+      onClick={toggleHandler}
+      className='hover:bg-primary border-b-0 border-l-0 rounded-b-none rounded-l-none'
+    >
+      <Modal open={open} setOpen={setOpen} className='i-modal'>
+        {closeHandler => (
+          <article className='size-full bg-primary border border-neutral rounded-md'>
+            <header className='flex items-center justify-between gap-x-6 h-10 border-b border-neutral px-4'>
+              <span className='capitalize'>{title}</span>
+              <button type='button' onClick={closeHandler}>
+                <X />
+              </button>
+            </header>
+            {children}
+          </article>
+        )}
+      </Modal>
+      <Expand />
+    </Button>
+  );
+};
+
+export default ExpandButton;
