121935ce8b2a4c11a57e5387db6468d78bfaf3ec
sina-byn <sina.sina.bayan@protonmail.com>
2024-07-01 21:04:50 +0330

    tooltip made multi-container and styles fixed

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -5,12 +5,16 @@ import { Tooltip as ReactTooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
 
 const Tooltip = () => {
+  const tooltipClassName = `
+    max-w-[260px] !bg-primary border border-neutral !rounded-md
+    whitespace-pre-wrap [word-break:break-word] !opacity-100 overflow-hidden
+  `;
+
   return (
-    <ReactTooltip
-      noArrow
-      id='info-tooltip'
-      className='max-w-[260px] !bg-primary border border-neutral !rounded-md whitespace-pre-wrap [word-break:break-word] !opacity-100 overflow-hidden'
-    />
+    <>
+      <ReactTooltip noArrow id='tooltip' className={tooltipClassName} />
+      <ReactTooltip noArrow id='info-tooltip' className={tooltipClassName} />
+    </>
   );
 };
 
