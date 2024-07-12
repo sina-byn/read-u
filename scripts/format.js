8e6bb9f94292510b9b3a511743a9e9651ef33a98
sina-byn <sina.sina.bayan@protonmail.com>
2024-07-12 16:37:32 +0330

    format script updated to exclude itself and invalid file extensions

--- scripts/format.js
+++ scripts/format.js
@@ -1,17 +1,24 @@
+import path from 'path';
 import cp from 'child_process';
+
+// * data
+const INVALID_EXTENSIONS = ['.ico', '.gif', '.svg', '.png', '.jpg', '.jpeg', '.jfif', '.md'];
 
 (async () => {
   const stagedFiles = cp
     .execSync('git diff --cached --name-only --diff-filter=ACM', { encoding: 'utf-8' })
     .split(/\s*\r*\n+\s*/g)
-    .filter(Boolean);
+    .filter(f => Boolean(f) && !INVALID_EXTENSIONS.includes(path.extname(f)));
 
   if (!stagedFiles.length) return;
 
   cp.execSync('git restore --staged .', { stdio: 'inherit' });
 
   for (const file of stagedFiles) {
-    cp.execSync(`npx prettier ${file} --write`, { stdio: 'inherit' });
+    if (!file.endsWith('scripts/format.js')) {
+      cp.execSync(`npx prettier ${file} --write`, { stdio: 'inherit' });
+    }
+
     cp.execSync(`git add ${file}`, { stdio: 'inherit' });
   }
 })();
