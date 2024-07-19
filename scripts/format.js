6c2b71cb953d514e5f7b5e8322760d6e381fa637
sina-byn <sina.sina.bayan@protonmail.com>
2024-07-19 21:46:21 +0330

    format script fixed to exclude files without extensions

--- scripts/format.js
+++ scripts/format.js
@@ -2,20 +2,35 @@ import path from 'path';
 import cp from 'child_process';
 
 // * data
-const INVALID_EXTENSIONS = ['.ico', '.gif', '.svg', '.png', '.jpg', '.jpeg', '.jfif', '.md'];
+const INVALID_EXTENSIONS = [
+  '.ico',
+  '.gif',
+  '.svg',
+  '.png',
+  '.jpg',
+  '.jpeg',
+  '.jfif',
+  '.md',
+  '.woff2',
+  '.template',
+];
 
 (async () => {
   const stagedFiles = cp
     .execSync('git diff --cached --name-only --diff-filter=ACM', { encoding: 'utf-8' })
     .split(/\s*\r*\n+\s*/g)
-    .filter(f => Boolean(f) && !INVALID_EXTENSIONS.includes(path.extname(f)));
+    .filter(Boolean);
 
   if (!stagedFiles.length) return;
 
   cp.execSync('git restore --staged .', { stdio: 'inherit' });
 
   for (const file of stagedFiles) {
-    if (!file.endsWith('scripts/format.js')) {
+    if (
+      path.extname(file) !== '' &&
+      !INVALID_EXTENSIONS.includes(path.extname(file)) &&
+      !file.endsWith('scripts/pre-commit.js')
+    ) {
       cp.execSync(`npx prettier ${file} --write`, { stdio: 'inherit' });
     }
 
