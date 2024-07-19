d58c017fa5ad21b3a850b58470b87a2ea34a4826
sina-byn <sina.sina.bayan@protonmail.com>
2024-07-19 12:39:04 +0330

    tab-sync guide modal made responsive

--- src/components/TabSyncGuide.tsx
+++ src/components/TabSyncGuide.tsx
@@ -28,9 +28,13 @@ const TabSyncGuide = () => {
   }, []);
 
   return (
-    <Modal open={open} setOpen={setOpen} className='i-modal'>
-      <article className='size-full bg-primary border border-neutral rounded-md'>
-        <header className='flex items-center justify-between gap-x-6 h-12 border-b border-neutral px-3'>
+    <Modal
+      open={open}
+      setOpen={setOpen}
+      className='flex justify-center items-center max-h-[calc(100svh_-_32px)] w-full max-w-[410px] overflow-hidden'
+    >
+      <article className='w-full h-fit bg-primary border border-neutral rounded-md overflow-hidden'>
+        <header className='flex items-center justify-between gap-x-2 h-12 border-b border-neutral px-3'>
           <div className='flex items-center gap-x-2'>
             <span className='capitalize'>tab synchronization guide</span>
           </div>
@@ -40,42 +44,43 @@ const TabSyncGuide = () => {
           </button>
         </header>
 
-        <div className='flex flex-col gap-y-3 min-h-[calc(100%_-_3rem)] p-3'>
-          <div>
-            <div className='flex gap-x-2 w-fit bg-info/30 rounded-md p-2'>
-              <Info size={18} className='shrink-0 text-info' />
-              <p className='text-sm -mt-0.5'>This app fully supports tab synchronization.</p>
+        <div className='content max-h-[calc(100svh_-_5rem_-_2px)] overflow-y-auto'>
+          <div className='flex flex-col gap-y-3 p-3'>
+            <div>
+              <div className='flex gap-x-2 w-fit bg-info/30 rounded-md p-2'>
+                <Info size={18} className='shrink-0 text-info' />
+                <p className='text-sm -mt-0.5'>This app fully supports tab synchronization.</p>
+              </div>
             </div>
-          </div>
 
-          <p>
-            Open this web app in another tab and start typing in either of the open tabs. You will
-            see that full tab synchronization is supported. Note that the table editor also supports
-            this feature.
-          </p>
+            <p>
+              Open this web app in another tab and start typing in either of the open tabs. You will
+              see that full tab synchronization is supported. Note that the table editor also
+              supports this feature.
+            </p>
 
-          <div className='flex flex-wrap items-center'>
-            To further enhance your experience, make sure to use
-            <div className='flex items-center justify-center w-fit h-10 bg-primary-light border border-neutral rounded-md scale-75 px-3'>
-              <PictureInPicture size={22} className='shrink-0 -scale-y-100' />
+            <div className='flex flex-col'>
+              To further enhance your experience, make sure to use
+              <div className='flex items-center justify-center w-fit h-10 bg-primary-light border border-neutral rounded-md scale-75 px-3 -ml-1.5'>
+                <PictureInPicture size={22} className='shrink-0 -scale-y-100' />
+              </div>
             </div>
-            .
+
+            <div className='grow grid md:grid-cols-2 gap-3'>
+              {/* eslint-disable-next-line */}
+              <img alt='tab sync guide' src='/images/editor-sync.gif' className='w-full' />
+              {/* eslint-disable-next-line */}
+              <img alt='tab sync guide' src='/images/preview-sync.gif' className='w-full' />
+            </div>
+
+            <footer>
+              <Checkbox onChange={showAgainDisableHandler}>Don't show again</Checkbox>
+
+              <Button variant='secondary' onClick={closeHandler} className='text-sm h-9 px-5 mt-2'>
+                Ok
+              </Button>
+            </footer>
           </div>
-
-          <div className='grow grid grid-cols-2 gap-3'>
-            {/* eslint-disable-next-line */}
-            <img alt='tab sync guide' src='/images/editor-sync.gif' />
-            {/* eslint-disable-next-line */}
-            <img alt='tab sync guide' src='/images/preview-sync.gif' />
-          </div>
-
-          <footer>
-            <Checkbox onChange={showAgainDisableHandler}>Don't show again</Checkbox>
-
-            <Button variant='secondary' onClick={closeHandler} className='text-sm h-9 px-5 mt-2'>
-              Ok
-            </Button>
-          </footer>
         </div>
       </article>
     </Modal>
